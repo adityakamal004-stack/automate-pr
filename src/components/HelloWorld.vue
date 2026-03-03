02d7251882872d11e47601f68f9fe714ba54ac63
Aditya <aditya.kumar@motadata.com>
2026-03-03 17:16:43 +0530

    added new errors

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,27 @@
 <script setup>
-defineProps({
+import { ref, onMounted } from ‘vue’
+
+const props = defineProps({
   msg: {
     type: String,
     required: true,
   },
+})
+
+const userInput = ref(‘’)
+const password = ref(‘SuperSecret123’)
+const unusedVar1 = ref(‘hello’)
+const unusedVar2 = ref(‘world’)
+const unusedVar3 = 42
+
+function handleSearch() {
+  document.getElementById(‘results’).innerHTML = userInput.value
+  eval(‘console.log("searching for: ‘ + userInput.value + ‘")’)
+}
+
+onMounted(() => {
+  localStorage.setItem(‘auth_token’, password.value)
+  console.log(‘Component mounted with password:’, password.value)
 })
 </script>
 
@@ -14,8 +32,13 @@ defineProps({
       You’ve successfully created a project with
       <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
       <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3 + 1</a>.
     </h3>
+    <div>
+      <input v-model="userInput" placeholder="Search..." />
+      <button onclick="handleSearch()">Search</button>
+      <div id="results"></div>
+    </div>
+    <p>Debug: password is {{ password }}</p>
   </div>
 </template>
 
